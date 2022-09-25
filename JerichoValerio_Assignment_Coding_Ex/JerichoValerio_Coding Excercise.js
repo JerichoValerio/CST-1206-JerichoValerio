a667d50d77cb005d504d3ac18b9bc4f9239506bb
JerichoValerio <jerichotvalerio@gmail.com>
2022-09-25 03:26:36 -0700

    done for now

--- JerichoValerio_Assignment_Coding_Ex/JerichoValerio_Coding Excercise.js
+++ JerichoValerio_Assignment_Coding_Ex/JerichoValerio_Coding Excercise.js
@@ -130,4 +130,29 @@ console.log
 
 // 8. Write a function to reverse a string
 
+<script>
+function ReverseString(str) {
+
+	// Check input
+	if(!str || str.length < 2 ||
+			typeof str!== 'string') {
+		return 'Not valid';
+	}
+	
+	// Take empty array revArray
+	const revArray = [];
+	const length = str.length - 1;
+	
+	// Looping from the end
+	for(let i = length; i >= 0; i--) {
+		revArray.push(str[i]);
+	}
+	
+	// Joining the array elements
+	return revArray.join('');
+
+}
+
+document.write(ReverseString("APPLE Is Better Than SAMSUNG"))
+</script>
 
