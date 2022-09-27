c9a14328a1853d5fe0970b460f81238de6be1c7a
Jericho T Valerio <jerichotvalerio@gmail.com>
2022-09-27 10:09:48 -0700

    added "css' to nptes

--- Class Notes/week5/week5_notes.js
+++ Class Notes/week5/week5_notes.js
@@ -1,18 +1,16 @@
-// WAY 1 capture the button element in script
+// Way 1 capture the button element in script
 
 // cont button = document.getElementById("click");
 // console.log(button);
 
-// WAY2
+// Way 2
 //const button = document.getElementsByTagName("button");
 // console.log(button);
-
 
 //way 3
 
 // const button = document.querySelector(".btn");
 // console.log(button)
-
 
 //way 4
 
@@ -21,3 +19,26 @@ console.log(button1);
 
 const buttons = document.querySelectorAll(".btn");
 console.log(buttons, "All");
+
+// event handler and listener for button 1
+
+ //button1.addEventListener("click", function () {
+//   console.log("fIRED");
+// });
+
+
+const container = document.querySelector(".container");
+console.log(container)
+
+
+container.addEventListener("mouseenter", () => {
+    console.log("mouse entered")
+})
+
+function mouseLeft() {
+    console.log("mouse left");
+}
+
+container.addEventListener("mouseleave", () => {
+    console.log("mouse left")
+})
