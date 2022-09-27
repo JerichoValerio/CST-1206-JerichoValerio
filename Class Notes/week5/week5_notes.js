7d48395df7a7238a36315f00f457bc5f5d8ecfcb
Jericho T Valerio <jerichotvalerio@gmail.com>
2022-09-27 09:46:10 -0700

    added button ways

---
+++ Class Notes/week5/week5_notes.js
@@ -0,0 +1,23 @@
+// WAY 1 capture the button element in script
+
+// cont button = document.getElementById("click");
+// console.log(button);
+
+// WAY2
+//const button = document.getElementsByTagName("button");
+// console.log(button);
+
+
+//way 3
+
+// const button = document.querySelector(".btn");
+// console.log(button)
+
+
+//way 4
+
+const button1 = document.querySelector(".btn");
+console.log(button1);
+
+const buttons = document.querySelectorAll(".btn");
+console.log(buttons, "All");
