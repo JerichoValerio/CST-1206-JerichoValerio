45fcfd2a41f8b5f5a99fcc942fd09291db085495
JerichoValerio <jerichotvalerio@gmail.com>
2022-10-03 07:17:07 -0700

    added a lofi moving background: my signature

--- JerichoValerio_Assignment_Events/Jericho_AS_Events.js
+++ JerichoValerio_Assignment_Events/Jericho_AS_Events.js
@@ -1,7 +1,7 @@
 function testVariable() {
   var strText = document.getElementById("mail").value;
   var strText1 = document.getElementById("name").value;
-  var strText2 = document.getElementById("pass").value;
-  var result = 'Email: ' + strText + ' ' + ' Name: ' + strText1 + ' ' + ' Password: ' + strText2;
+  var strText2 = document.getElementById("pass").value; 
+  var result = 'Email: ' + strText + ' ' +  + ' ' +' Name: ' + strText1 + ' ' +  + ' ' + ' Password: ' + strText2;
   document.getElementById("spanResult").textContent = result;
 }
