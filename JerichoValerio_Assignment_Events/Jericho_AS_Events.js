10875ec48b41670e88716a3b185ebc63dc9dcbaa
JerichoValerio <jerichotvalerio@gmail.com>
2022-10-03 07:40:43 -0700

    testing out code

--- JerichoValerio_Assignment_Events/Jericho_AS_Events.js
+++ JerichoValerio_Assignment_Events/Jericho_AS_Events.js
@@ -2,6 +2,6 @@ function testVariable() {
   var strText = document.getElementById("mail").value;
   var strText1 = document.getElementById("name").value;
   var strText2 = document.getElementById("pass").value; 
-  var result = 'Email: ' + strText + ' ' +  + ' ' +' Name: ' + strText1 + ' ' +  + ' ' + ' Password: ' + strText2;
+  var result = 'Email: ' + strText  + ' ' +' Name: ' + strText1  + ' ' + ' Password: ' + strText2;
   document.getElementById("spanResult").textContent = result;
 }
