126f22c75626478b254b36f31539d65b465a6d2b
Jericho T Valerio <jerichotvalerio@gmail.com>
2022-10-11 09:42:48 -0700

    examples

--- Class Notes/week_7/week_7.js
+++ Class Notes/week_7/week_7.js
@@ -6,9 +6,56 @@ var name = 'Daniel'
 
 let obj = {
   name: 'Jericho',
-  getName: function () {
+  getName: () => {
     console.log(this.name)
   }
 }
 
 obj.getName()
+
+// main difference on this keyword
+
+let obj2 = {
+  name: 'Jericho',
+  getName: () => {
+    console.log(this.name)
+  }
+}
+
+obj2.getName()
+
+var game = 'cod'
+let obj2 = {
+  game: 'val',
+  getGame: () => {
+    console.log(this.game)
+  }
+}
+
+obj2.getGame()
+
+
+var city = {
+
+}
+let student2 = {
+  name: "dan",
+  college: 'vcc',
+  city: "Tronto",
+  addy: {
+    city: 'van',
+    province: 'bc',
+    getCity: function () {
+      console.log(this.city)
+    }
+  }
+}
+student.addy.getCity(); 
+
+
+
+// hositing 
+console.log(collegeName); //undefined
+
+var collegeName = "VCC";
+
