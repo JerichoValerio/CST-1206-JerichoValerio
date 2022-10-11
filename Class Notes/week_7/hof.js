7d3b9de8532fa97d1aa2821143bc0511bb3d4a9d
Jericho T Valerio <jerichotvalerio@gmail.com>
2022-10-11 10:50:19 -0700

    modifeid map array

--- Class Notes/week_7/hof.js
+++ Class Notes/week_7/hof.js
@@ -32,3 +32,54 @@ function printVAlue(item, index) {
 }
 
 array.forEach(printVAlue);
+
+// let array = [1, 2, 3, 4, 5];
+
+// //map function
+// let newArray = array.map((item, index) => {
+//   return item * 2;
+// });
+//********************* */
+
+//^^^^^
+
+let numArray = [1, 2, 3, 4, 5];
+let newArray = numArray.map(function(item, index) {
+  return item * 2;
+});
+
+console.log(numArray, "ORIGINAL ARRAY");
+console.log(newArray, "NEW ARRAY");
+// using the map function, length array is same
+
+
+//change each value either pass or fail
+let studentMathMarks = [
+    {
+        name: "dan",
+        roleNumeber: 100,
+        grade: 90
+    },
+    {
+        name: "dang",
+        roleNumeber: 101,
+        grade: 40
+    },
+    {
+        name: "dayy",
+        roleNumeber: 103,
+        grade: 70
+    }
+]
+
+let modifiedArray = studentMathMarks.map((student) => {
+    if (student.grade > 50) {
+        student.grade = "PASS"
+
+    } else {
+        student.grade = "FAIL"
+    }
+    return student
+})
+
+console.log(modifiedArray)
