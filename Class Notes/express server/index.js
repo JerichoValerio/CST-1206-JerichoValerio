a4cefb4e2d070da0df7da966416edf0bc688e410
Jericho T Valerio <jerichotvalerio@gmail.com>
2022-10-11 12:25:20 -0700

    showing status notes added soon , for student soon

--- Class Notes/express server/index.js
+++ Class Notes/express server/index.js
@@ -1,16 +1,19 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const PORT = 4000;
+const student = [];
 
-app.get('/', (req, res) => {
-    res.send("Welcome!");
-})
+app.get("/", (req, res) => {
+  res.send("Welcomeyo student APIS!");
+});
 
-app.get('/user', (req, res) => {
-    res.send("Hello User!");
+app.get('/students', () => {
+    // for get requests we give 200 success response
+   const data = res.status(200);
+   return data.json(student)
 })
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
+  console.log(`Server running on port ${PORT}`);
+});
