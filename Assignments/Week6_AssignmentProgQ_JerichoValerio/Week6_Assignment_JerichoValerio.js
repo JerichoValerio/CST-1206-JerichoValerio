6f893542dd4c45b2ada65c9d52ea00b2259d9622
JerichoValerio <jerichotvalerio@gmail.com>
2022-10-14 11:41:29 -0700

    reformatted code, cleaner

--- Assignments/Week6_AssignmentProgQ_JerichoValerio/Week6_Assignment_JerichoValerio.js
+++ Assignments/Week6_AssignmentProgQ_JerichoValerio/Week6_Assignment_JerichoValerio.js
@@ -140,21 +140,3 @@ const findMissing = (arr = []) => {
 console.log(findMissing(arr))
 
 //************************************************************************************************
-//   Question 3
-//   Write a Express server where you will keep records of Employees
-//   Write get and post api’s for getting all the employees and adding employee to the list
-//   The object of Employee should look like
-//   {
-//   “Name” : “Daniel”,
-//   “employeeId”: 100,
-//   “Email” : “daniel@gmail.com”,
-//   “Department” :”IT”
-//   }
-//   Please submit the code via GITHUB Repo.
-//   Add the link of Github repo while submitting your code.
-//   Your repository should have 2 folders
-//   1. Programming questions (For first 2)
-//   2. Express (For Express Application)
-//! BRO EXPRESS IS HARD :)))
-
-
