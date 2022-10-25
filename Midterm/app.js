f1c47680fa6a14fd6152f1a9b2e08a997bb63629
Jericho Valerio <72720530+JerichoValerio@users.noreply.github.com>
2022-10-25 09:17:24 -0700

    added the things i need for midterm

---
+++ Midterm/app.js
@@ -0,0 +1,20 @@
+
+
+let employeeList = [
+  {
+    id: 1,
+    name: 'Mike',
+    email: 'mike@company.ca',
+    employeeID: 100,
+    department: 'IT',
+    Salary: 100000
+  },
+  {
+    id: 2,
+    name: 'Daniel',
+    email: 'daniel@company.ca',
+    employeeID: 101,
+    department: 'HR',
+    Salary: 50000
+  }
+]
