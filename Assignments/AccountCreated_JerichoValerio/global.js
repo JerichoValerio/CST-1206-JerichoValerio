06180c5ae6f27f264f8239177cb54493b0b843c6
JerichoValerio <jerichotvalerio@gmail.com>
2022-10-30 15:06:16 -0700

    watching prabh's zoom rec

--- Assignments/AccountCreated_JerichoValerio/global.js
+++ Assignments/AccountCreated_JerichoValerio/global.js
@@ -2,3 +2,7 @@
 // for example github.com -> 4 different projects(your code is inside one repo)
 // what if repo deleted, all are gone == monolith architecture <- if one server is done, there is still more
 // can assign different engineers to different servers
+
+const express = require("express");
+const app = express();
+const PORT = 5000;
