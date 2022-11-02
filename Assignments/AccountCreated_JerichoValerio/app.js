cd69a972d5cb0d5851aa1875ce224c73119d9009
JerichoValerio <jerichotvalerio@gmail.com>
2022-11-02 03:08:47 -0700

    managed to get it dine, not final commit though...

--- Assignments/AccountCreated_JerichoValerio/app.js
+++ Assignments/AccountCreated_JerichoValerio/app.js
@@ -4,23 +4,33 @@
 // can assign different engineers to different servers
 
 const express = require("express");
+const bodyParser = require("body-parser");
+const { check, validationResult } = require("express-validator");
 const app = express();
 const PORT = 5000;
 
 app.set("view engine", "ejs");
 
-// app.get("/", (req, res) => {
-//   res.render("index", { name: "Jericho" });
-// });
+app.get("", (req, res) => {
+  res.render("index");
+});
 
-//let listOfNames = ["Jericho", "John", "Jane", "Jill"];
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
-// let html = "<h1>Hello, I am HTML</h1>";
+app.post("/register", (req, res) => {
+  res.render("success");
+});
 
-// app.get("/", (req, res) => {
-//   res.render("index", { name: html });
-// });
+app.get("/success", (req, res) => {
+  res.render("success", {
+    name: "Jericho",
+    email: "jerichovalerio@noogler.com",
+    contactnumber: "778-721-0831",
+  });
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server Running at port ${PORT}`);
 });
